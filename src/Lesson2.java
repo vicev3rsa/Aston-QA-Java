ae146bcddb61083a2faee5aaa97d5253cccf6fff
Вера <verakoroleva99@gmail.com>
2024-05-20 18:41:45 +0300

    L1

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,5 +1,9 @@
 public class Lesson2 {
     public static void main(String[] args) {
+        int x=10;
+        int y=25;
+        int z=x+y;
 
+        System.out.println("Sum of x+y = " + z);
     }
 }
